0409c63ac435834c00058c4b312f1ab339430427
Douglas Marcondes <71682660+dmarck82@users.noreply.github.com>
2023-09-15 14:25:05 -0300

    Validações e tratamento de erros
    Adicionado as validações e tratamento para as mensagens de erros nos controllers.

---
+++ src/main/java/br/edu/utfpr/turismoapi/controllers/AuthController.java
@@ -0,0 +1,44 @@
+package br.edu.utfpr.turismoapi.controllers;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.utfpr.turismoapi.dto.AuthDTO;
+import br.edu.utfpr.turismoapi.security.JwtUtil;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping
+    public ResponseEntity<Object> auth(@Valid @RequestBody AuthDTO authDTO){
+        
+        System.out.println(authDTO);
+
+        //Dados do usuário (Payload)
+        var payload = new HashMap<String, Object>();
+        payload.put("username", authDTO.username);
+
+        var now = Instant.now();
+
+        var jwt = jwtUtil.generateToken(payload, "1234", 3600);
+
+        var res = new HashMap<String, Object>();
+        res.put("token", jwt);
+        res.put("issuedIn", now);
+        res.put("expiresIn", now.plus(3600, ChronoUnit.SECONDS));
+
+        return ResponseEntity.ok().body(jwt);
+    }
+}
--- src/main/java/br/edu/utfpr/turismoapi/dto/PasseioDTO.java
+++ src/main/java/br/edu/utfpr/turismoapi/dto/PasseioDTO.java
@@ -1,5 +1,7 @@
 package br.edu.utfpr.turismoapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +16,14 @@ import lombok.ToString;
 
 public class PasseioDTO {
     
+    @NotBlank
+    @Size(min = 2, max = 50)
     private String destino;
+
+    @NotBlank
+    @Size(min = 2, max = 150)
     private String itinerario;
+
+    @NotBlank
     private double preco;
 }
