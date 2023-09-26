07d072ade4113cac041cf3ff451e5ff40b903026
Douglas Marcondes <71682660+dmarck82@users.noreply.github.com>
2023-09-26 20:34:52 -0300

    4ª-Entrega
    Adicionado medidas de segurança na API

--- src/main/java/br/edu/utfpr/turismoapi/repositories/PessoaRepository.java
+++ src/main/java/br/edu/utfpr/turismoapi/repositories/PessoaRepository.java
@@ -1,5 +1,7 @@
 package br.edu.utfpr.turismoapi.repositories;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +10,16 @@ import br.edu.utfpr.turismoapi.models.Pessoa;
 
 public interface PessoaRepository extends JpaRepository <Pessoa, UUID> {
     
+    public boolean existsByIdAndEmail(UUID id, String email);
 
+    public boolean existsByEmail(String email);
+
+    public boolean existsByEmailAndSenha(String email, String senha);
+
+    public Optional<Pessoa> findByEmail(String email);
+
+    public Optional<Pessoa> findByEmailAndSenha(String email, String senha);
+
+    public List<Pessoa> findByNome(String nome);
 
 }
---
+++ src/main/java/br/edu/utfpr/turismoapi/services/TokenUserDetailService.java
@@ -0,0 +1,20 @@
+package br.edu.utfpr.turismoapi.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import br.edu.utfpr.turismoapi.repositories.PessoaRepository;
+
+@Component
+public class TokenUserDetailService implements UserDetailsService {
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return pessoaRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
